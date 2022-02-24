65fb79f50267014f30f3101009554e598d59600b
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-02-24 18:22:43 +0700

    draft

---
+++ 3.1.py
@@ -0,0 +1,34 @@
+from turtle import *
+
+class Disk(object):
+    def __init__(self, name = "", xpos = 0, ypos = 0, height = 20, width = 40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+
+        self.t = Turtle()
+        self.t.fillcolor("blue")
+
+    def showdisk(self):
+        self.t.pu()
+        self.t.setposition(self.dxpos, self.dypos)
+        self.t.pd()
+        self.t.begin_fill
+        
+        for i in range(2):
+            self.t.forward(self.dwidth/2)
+            self.t.left(self.dheight/2)
+            self.t.forward(self.dwidth/2)
+            self.t.left(self.dheight/2)
+        
+        self.t.end_fill
+        #self.t.pu()
+        
+    def newpos(self, xpos, ypos):
+        self.dxpos = xpos
+        self.dypos = ypos
+        
+    def cleardisk(self):
+        self.t.clear
---
+++ 3.2.py
@@ -0,0 +1,29 @@
+from turtle import *
+
+class Pole(object):
+    def __init__(self, name = "", xpos = 0, ypos = 0, thick = 10, length = 100):
+        self.pname = name
+        self.stack = []
+        self.toppos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+
+        self.t = Turtle()
+        self.t.fillcolor("green")
+
+    def showpole(self):
+        self.t.pu()
+        self.t.setposition(self.pxpos, self.pypos)
+        self.t.pd()
+        self.t.begin_fill
+        
+        for i in range(2):
+            self.t.forward(self.pthick/2)
+            self.t.left(self.plength/2)
+            self.t.forward(self.pthick/2)
+            self.t.left(self.plength/2)
+        
+        self.t.end_fill
+        #self.t.pu()
