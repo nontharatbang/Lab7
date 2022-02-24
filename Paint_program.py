6d53c3bf794715103a2861d884d3fa0f35e3d0d8
nontharatbang <73401553+nontharatbang@users.noreply.github.com>
2022-02-24 17:27:54 +0700

    edit

--- Paint_program.py
+++ Paint_program.py
@@ -1,43 +1,85 @@
 import sys
-from PyQt5.QtCore import Qt, QPoint
-from PyQt5.QtWidgets import QMainWindow, QApplication
-from PyQt5.QtGui import QPixmap, QPainter, QPen
+from PySide6 import QtCore, QtGui, QtWidgets
+from PySide6.QtCore import Qt
 
 
-class Menu(QMainWindow):
+class Canvas(QtWidgets.QLabel):
 
     def __init__(self):
         super().__init__()
-        self.drawing = False
-        self.lastPoint = QPoint()
-        self.image = QPixmap("white.png")
-        self.setGeometry(100, 100, 500, 300)
-        self.resize(self.image.width(), self.image.height())
-        self.show()
+        pixmap = QtGui.QPixmap(600, 300)
+        self.setPixmap(pixmap)
 
-    def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.drawPixmap(self.rect(), self.image)
+        self.last_x, self.last_y = None, None
+        self.pen_color = QtGui.QColor('#000000')
 
-    def mousePressEvent(self, event):
-        if event.button() == Qt.LeftButton:
-            self.drawing = True
-            self.lastPoint = event.pos()
+    def set_pen_color(self, c):
+        self.pen_color = QtGui.QColor(c)
 
-    def mouseMoveEvent(self, event):
-        if event.buttons() and Qt.LeftButton and self.drawing:
-            painter = QPainter(self.image)
-            painter.setPen(QPen(Qt.red, 3, Qt.SolidLine))
-            painter.drawLine(self.lastPoint, event.pos())
-            self.lastPoint = event.pos()
-            self.update()
+    def mouseMoveEvent(self, e):
+        if self.last_x is None: # First event.
+            self.last_x = e.x()
+            self.last_y = e.y()
+            return # Ignore the first time.
 
-    def mouseReleaseEvent(self, event):
-        if event.button == Qt.LeftButton:
-            self.drawing = False
+        canvas = self.pixmap()
+        painter = QtGui.QPainter(canvas)
+        p = painter.pen()
+        p.setWidth(4)
+        p.setColor(self.pen_color)
+        painter.setPen(p)
+        painter.drawLine(self.last_x, self.last_y, e.x(), e.y())
+        painter.end()
+        self.setPixmap(canvas)
+
+        # Update the origin for next time.
+        self.last_x = e.x()
+        self.last_y = e.y()
+
+    def mouseReleaseEvent(self, e):
+        self.last_x = None
+        self.last_y = None
+COLORS = [
+# 17 undertones https://lospec.com/palette-list/17undertones
+'#000000', '#141923', '#414168', '#3a7fa7', '#35e3e3', '#8fd970', '#5ebb49',
+'#458352', '#dcd37b', '#fffee5', '#ffd035', '#cc9245', '#a15c3e', '#a42f3b',
+'#f45b7a', '#c24998', '#81588d', '#bcb0c2', '#ffffff',
+]
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    mainMenu = Menu()
-    sys.exit(app.exec_())
+class QPaletteButton(QtWidgets.QPushButton):
+
+    def __init__(self, color):
+        super().__init__()
+        self.setFixedSize(QtCore.QSize(24,24))
+        self.color = color
+        self.setStyleSheet("background-color: %s;" % color)
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.canvas = Canvas()
+
+        w = QtWidgets.QWidget()
+        l = QtWidgets.QVBoxLayout()
+        w.setLayout(l)
+        l.addWidget(self.canvas)
+
+        palette = QtWidgets.QHBoxLayout()
+        self.add_palette_buttons(palette)
+        l.addLayout(palette)
+
+        self.setCentralWidget(w)
+
+    def add_palette_buttons(self, layout):
+        for c in COLORS:
+            b = QPaletteButton(c)
+            b.pressed.connect(lambda c=c: self.canvas.set_pen_color(c))
+            layout.addWidget(b)
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+app.exec_()
